7f287f85a93cc1e40153aa4197c33dd4a864914a
Catalina Tarelli <catatarelli@hotmail.com>
2022-10-12 00:23:03 +0200

    Add variable to characters file

--- src/characters/characters.ts
+++ src/characters/characters.ts
@@ -1,37 +1,20 @@
-const personajes = [
-  {
-    name: "Joffrey",
-    family: "Baratheon",
-    age: 16,
-    yearsOfReign: 2,
-  },
-  {
-    name: "Jaime",
-    family: "Lannister",
-    age: 45,
-    weapon: "Espada",
-    destreza: 7,
-  },
-  {
-    name: "Daenerys",
-    family: "Targaryen",
-    age: 30,
-    weapon: "Dragones",
-    destreza: 10,
-  },
-  {
-    name: "Tyrion",
-    family: "Lannister",
-    age: 40,
-    asesorado: "Daenerys",
-  },
-  {
-    nombre: "Bronn",
-    family: "AguasNegras",
-    age: 54,
-    sirveA: "Jaime",
-    pelotismo: 0,
-  },
-];
+import Advisor from "./Advisor/Advisor.js";
+import Fighter from "./Fighter/Fighter.js";
+import King from "./King/King.js";
+import Squire from "./Squire/Squire.js";
 
-export default personajes;
+const joffrey = new King("Joffrey", "Baratheon", 16, 2);
+const jaime = new Fighter("Jaime", "Lannister", 45, "Espada", 7);
+const daenerys = new Fighter("Daenerys", "Targaryen", 30, "Dragones", 10);
+const tyrion = new Advisor("Tyrion", "Lannister", 40, "Daenerys");
+const bronn = new Squire("Bronn", "AguasNegras", 54, 0, "Jaime");
+
+joffrey.imagePath = "../../images/joffrey.jpg";
+jaime.imagePath = "../../images/jaime.jpg";
+daenerys.imagePath = "../../images/daenerys.jpg";
+tyrion.imagePath = "../../images.tyrion.jpg";
+bronn.imagePath = "../../images.bronn.jpg";
+
+const characters = [joffrey, jaime, daenerys, tyrion, bronn];
+
+export default characters;
